0e9635c2df2bbe257bbd5a4f7c83d06dcb6930ff
Vipul Rahane <vipul.rahane@juul.com>
2026-04-24 12:51:10 -0700

    TRNG fix

--- hw/drivers/trng/trng_da1469x/src/trng_da1469x.c
+++ hw/drivers/trng/trng_da1469x/src/trng_da1469x.c
@@ -18,12 +18,44 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <trng/trng.h>
 #include <mcu/mcu.h>
 #include "mcu/da1469x_clock.h"
 
 #define DA1469X_TRNG_FIFO_SIZE  (32 * sizeof(uint32_t))
 #define DA1469X_TRNG_FIFO_ADDR  (0x30050000UL)
+
+/*
+ * Maximum iterations to wait for the TRNG FIFO to fill before giving up.
+ * The TRNG normally fills in microseconds; this timeout (~100ms at 32MHz)
+ * prevents an infinite busy-wait if the hardware is stuck after a warm reset.
+ */
+#define DA1469X_TRNG_WAIT_MAX   (3200000)
+
+#define DA1469X_TRNG_MAX_RETRIES 2
+
+static bool
+da1469x_trng_wait_fifo(uint8_t level)
+{
+    uint32_t timeout = DA1469X_TRNG_WAIT_MAX;
+
+    while (TRNG->TRNG_FIFOLVL_REG < level) {
+        if (--timeout == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static void
+da1469x_trng_reset_hw(void)
+{
+    TRNG->TRNG_CTRL_REG = 0;
+    da1469x_clock_amba_disable(CRG_TOP_CLK_AMBA_REG_TRNG_CLK_ENABLE_Msk);
+    da1469x_clock_amba_enable(CRG_TOP_CLK_AMBA_REG_TRNG_CLK_ENABLE_Msk);
+    TRNG->TRNG_CTRL_REG = TRNG_TRNG_CTRL_REG_TRNG_ENABLE_Msk;
+}
 
 static size_t
 da1469x_trng_read(struct trng_dev *trng, void *ptr, size_t size)
@@ -33,6 +65,8 @@ da1469x_trng_read(struct trng_dev *trng, void *ptr, size_t size)
     uint8_t *ptr_u8 = ptr;
     uint32_t *ptr_u32 = ptr;
     uint32_t tmp;
+    uint8_t fifo_target;
+    int retries;
     int i;
 
     if (size > DA1469X_TRNG_FIFO_SIZE) {
@@ -42,13 +76,22 @@ da1469x_trng_read(struct trng_dev *trng, void *ptr, size_t size)
     words_to_generate = size / sizeof(uint32_t);
     remaining_octets = size % sizeof(uint32_t);
 
+    fifo_target = remaining_octets ? words_to_generate + 1 : words_to_generate;
+
     da1469x_clock_amba_enable(CRG_TOP_CLK_AMBA_REG_TRNG_CLK_ENABLE_Msk);
     TRNG->TRNG_CTRL_REG = TRNG_TRNG_CTRL_REG_TRNG_ENABLE_Msk;
 
-    if (remaining_octets) {
-        while (TRNG->TRNG_FIFOLVL_REG < words_to_generate + 1);
-    } else {
-        while (TRNG->TRNG_FIFOLVL_REG < words_to_generate);
+    for (retries = 0; retries < DA1469X_TRNG_MAX_RETRIES; retries++) {
+        if (da1469x_trng_wait_fifo(fifo_target)) {
+            break;
+        }
+        da1469x_trng_reset_hw();
+    }
+
+    if (retries == DA1469X_TRNG_MAX_RETRIES) {
+        TRNG->TRNG_CTRL_REG = 0;
+        da1469x_clock_amba_disable(CRG_TOP_CLK_AMBA_REG_TRNG_CLK_ENABLE_Msk);
+        return 0;
     }
 
     for (i = 0; i < words_to_generate; i++) {
@@ -74,10 +117,23 @@ static uint32_t
 da1469x_trng_get_u32(struct trng_dev *trng)
 {
     uint32_t ret;
+    int retries;
 
     da1469x_clock_amba_enable(CRG_TOP_CLK_AMBA_REG_TRNG_CLK_ENABLE_Msk);
     TRNG->TRNG_CTRL_REG = TRNG_TRNG_CTRL_REG_TRNG_ENABLE_Msk;
-    while (TRNG->TRNG_FIFOLVL_REG < 1);
+
+    for (retries = 0; retries < DA1469X_TRNG_MAX_RETRIES; retries++) {
+        if (da1469x_trng_wait_fifo(1)) {
+            break;
+        }
+        da1469x_trng_reset_hw();
+    }
+
+    if (retries == DA1469X_TRNG_MAX_RETRIES) {
+        TRNG->TRNG_CTRL_REG = 0;
+        da1469x_clock_amba_disable(CRG_TOP_CLK_AMBA_REG_TRNG_CLK_ENABLE_Msk);
+        return 0;
+    }
 
     ret = *(uint32_t *)DA1469X_TRNG_FIFO_ADDR;
 
